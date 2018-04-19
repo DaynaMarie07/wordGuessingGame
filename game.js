8ea99e581d182da21d1c4c98c07199dcf52e6c43
dmarie <daynamarie@github.com>
2018-04-18 22:31:53 -0400

    ready to play a game

---
+++ game.js
@@ -0,0 +1,4 @@
+exports.newWord = {
+    wordList:['SUNFLOWERS', 'DAISY', 'ROSE', 'GERBER', 'LOTUS', 'BUTTERCUP', 'LILY', 'LARKSPUR', 'AMARYLLIS', 'FREESIA', 'DAFFODIL', 'DAHLIA', 'ORCHIDS', 'PEONY', 'TULIP', 'CROCUS', 'CARNATION', ],
+    hangman: ["\n\n\n\n\n\n\n----------", "\n |\n |\n |\n |\n |\n |\n |\n-----", "____________\n |         |\n |\n |\n |\n |\n |\n----","____________\n |         |\n |         O\n |\n |\n |\n |\n |\n------","____________\n |         |\n |         O\n |         |\n |         |\n |\n |\n |\n-----","____________\n |         |\n |         O\n |         |\n |         |\n |        ---------\n |\n |\n----", ]
+  }
